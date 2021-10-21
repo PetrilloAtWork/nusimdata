bc3702096cc3e286ba6729950b0fae326973ea46
Lynn Garren <garren@fnal.gov>
2021-10-20 19:49:06 -0500

    Minor tweaks to FindDk2nuData.cmake
        * Changed organization slightly to be sure of getting variables defined
          under all appropriate conditions.
    
        * Dk2nuData::dk2nuTree should be SHARED IMPORTED.
    
        * find_package_handle_standard_args() should be called unconditionally.

--- Modules/FindDk2nuData.cmake
+++ Modules/FindDk2nuData.cmake
@@ -4,25 +4,21 @@ FindDk2nuData
   find dk2nugenie
 
 #]================================================================]
-if (NOT Dk2nuData_FOUND)
-  if (Dk2nuData_FIND_REQUIRED)
-    set(_cet_Dk2nuData_FIND_REQUIRED ${Dk2nuData_FIND_REQUIRED})
-    unset(Dk2nuData_FIND_REQUIRED)
-  else()
-    unset(_cet_Dk2nuData_FIND_REQUIRED)
-  endif()
-  find_package(Dk2nuData CONFIG QUIET)
-  if (_cet_Dk2nuData_FIND_REQUIRED)
-    set(Dk2nuData_FIND_REQUIRED ${_cet_Dk2nuData_FIND_REQUIRED})
-    unset(_cet_Dk2nuData_FIND_REQUIRED)
-  endif()
-  if (Dk2nuData_FOUND)
-    set(_cet_Dk2nuData_config_mode CONFIG_MODE)
-  else()
-    unset(_cet_Dk2nuData_config_mode)
-  endif()
+if (Dk2nuData_FIND_REQUIRED)
+  set(_cet_Dk2nuData_FIND_REQUIRED ${Dk2nuData_FIND_REQUIRED})
+  unset(Dk2nuData_FIND_REQUIRED)
+else()
+  unset(_cet_Dk2nuData_FIND_REQUIRED)
 endif()
-if (NOT Dk2nuData_FOUND)
+find_package(Dk2nuData CONFIG QUIET)
+if (_cet_Dk2nuData_FIND_REQUIRED)
+  set(Dk2nuData_FIND_REQUIRED ${_cet_Dk2nuData_FIND_REQUIRED})
+  unset(_cet_Dk2nuData_FIND_REQUIRED)
+endif()
+if (Dk2nuData_FOUND)
+  set(_cet_Dk2nuData_config_mode CONFIG_MODE)
+else()
+  unset(_cet_Dk2nuData_config_mode)
   find_file(_cet_dk2nu_h NAMES dk2nu.h HINTS ENV DK2NUDATA_INC
     PATH_SUFFIXES dk2nu/tree)
   if (_cet_dk2nu_h)
@@ -40,38 +36,32 @@ if (NOT Dk2nuData_FOUND)
     if (_cet_Dk2nuData_dir STREQUAL "/")
       unset(_cet_Dk2nuData_dir)
     endif()
+    set(Dk2nuData_INCLUDE_DIRS "${_cet_Dk2nuData_include_dir}")
+    set(Dk2nuData_LIBRARY_DIR "${_cet_Dk2nuData_dir}/lib}")
+    find_library(Dk2nuData_LIBRARY NAMES dk2nuTree HINTS ${Dk2nuData_LIBRARY_DIR})
   endif()
 endif()
-if (Dk2nuData_FOUND AND _cet_Dk2nuData_include_dir)
-  if (TARGET ${CMAKE_FIND_PACKAGE_NAME}::dk2nuTree)
-    set(Dk2nuData_LIBRARY "${CMAKE_FIND_PACKAGE_NAME}::dk2nuTree")
-    target_include_directories(${Dk2nuData_LIBRARY} AFTER INTERFACE ${_cet_Dk2nuData_include_dir})
-  else()
-    message(STATUS "finding Dk2nuData_LIBRARY")
-    find_library(Dk2nuData_LIBRARY NAMES dk2nuTree PATHS ENV DK2NUDATA_LIB REQUIRED)
-    add_library(Dk2nuData::dk2nuTree UNKNOWN IMPORTED)
+if (Dk2nuData_FOUND)
+  if (NOT TARGET Dk2nuData::dk2nuTree)
+    add_library(Dk2nuData::dk2nuTree SHARED IMPORTED)
     set_target_properties(Dk2nuData::dk2nuTree PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${Dk2nuData_INCLUDE_DIRS}"
       IMPORTED_LOCATION "${Dk2nuData_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${Dk2nuData_INCLUDE_DIR}")
+      )
+    set(Dk2nuData_LIBRARY "Dk2nuData::dk2nuTree")
   endif()
-endif()
-message(STATUS "_cet_Dk2nuData_include_dir is ${_cet_Dk2nuData_include_dir}")
-include_directories("${_cet_Dk2nuData_include_dir}")
-set(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR "${_cet_Dk2nuData_include_dir}")
-set(Dk2nuData_FIND_REQUIRED ${_cet_Dk2nuData_FIND_REQUIRED})
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Dk2nuData
-  FOUND_VAR Dk2nuData_FOUND
-  REQUIRED_VARS
-    Dk2nuData_LIBRARY
-    Dk2nuData_INCLUDE_DIR
-)
-if(Dk2nuData_FOUND)
-  set(Dk2nuData_LIBRARIES ${Dk2nuData_LIBRARY})
-  set(Dk2nuData_INCLUDE_DIRS ${Dk2nuData_INCLUDE_DIR})
-  set(Dk2nuData_DIR "${_cet_Dk2nuData_dir}")
+  if (CETMODULES_CURRENT_PROJECT_NAME AND
+      ${CETMODULES_CURRENT_PROJECT_NAME}_OLD_STYLE_CONFIG_VARS)
+    include_directories("${Dk2nuData_INCLUDE_DIRS}")
+    set(DK2NUDATA "${Dk2nuData_LIBRARY")
+  endif()
 endif()
 
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Dk2nuData ${_cet_Dk2nuData_config_mode}
+  REQUIRED_VARS Dk2nuData_FOUND
+  Dk2nuData_INCLUDE_DIRS
+  Dk2nuData_LIBRARY)
 
 unset(_cet_Dk2nuData_FIND_REQUIRED)
 unset(_cet_Dk2nuData_config_mode)
